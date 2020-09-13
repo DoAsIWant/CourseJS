4a900a219b1f59d10d42129a1cccd7a871efbc49
DoAsIWant <danylo.kutsenko@nure.ua>
2020-09-13 21:29:33 +0300

    hometask1

---
+++ task1.js
@@ -0,0 +1,21 @@
+let firstDigit = parseInt(prompt("Введите первое число"));
+let secondDigit = parseInt(prompt("Введите второе число"));
+let operation = prompt("Введите операцию");
+let result = 0;
+
+switch(operation){
+    case "*" :
+      result = firstDigit * secondDigit;
+      break;
+    case "/" :
+       result = firstDigit / secondDigit;
+        break;
+    case "+" :
+        result = firstDigit + secondDigit;
+        break;
+    case "+" :
+        result = firstDigit + secondDigit;
+        break;                 
+}
+
+alert(result);
