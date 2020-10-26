a84f642193f23c1cce97dcabcc9e395ed9e2b0b1
DoAsIWant <danylo.kutsenko@nure.ua>
2020-10-26 11:40:51 +0200

    sevemth_homework

---
+++ seventh__homework/task1.js
@@ -0,0 +1,20 @@
+
+
+function decorator(fn){
+    let counter = 0;
+    fn();
+    return function(){
+      counter++;
+      console.log(counter);
+    }
+    
+}
+
+function log(){
+    console.log("Something");
+}
+
+let callFunct = decorator(log);
+callFunct();
+callFunct();
+
